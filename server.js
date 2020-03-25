36b1d2e5701de55adff185adab43cb7249d5d7e8
Jackiemn <59993378+Jackiemn@users.noreply.github.com>
2020-03-24 20:06:31 -0700

    passport authentication and validation

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+const passport = require('passport');
 const app = express();
 
 // Body parser middleware
@@ -18,6 +19,11 @@ mongoose
 .connect(db)
 .then(() => console.log('MongoDb connected!'))
 .catch(err => console.log(err));
+
+
+//Passport middleware
+app.use(passport.initialize());
+//Passport config
 
 
 //let's write our first route
